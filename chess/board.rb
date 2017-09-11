3794c65002a454ddc6196cd0f27748c387cd7d57
Emilio da Costa <emidac@gmail.com>
2017-09-11 18:03:07 -0400

    Phase 1 Chess Board Complete

--- chess/board.rb
+++ chess/board.rb
@@ -1,5 +1,7 @@
 require_relative "piece.rb"
 require "byebug"
+class BoardError < StandardError
+end
 
 class Board
 
@@ -44,7 +46,15 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-
+    # debugger
+    if self[start_pos].is_a?(NullPiece)
+      raise BoardError.new ("There is no piece at #{start_pos}.")
+    elsif (end_pos.first < 0) || (end_pos.first > 7) || (end_pos.last < 0) || (end_pos.last > 7)
+      raise BoardError.new ("This piece cannot move to #{end_pos}.")
+    end
+    self[end_pos] = self[start_pos]
+    self[start_pos] = NullPiece.new
+    # self[end_pos] = Piece #TODO needs to refer to actual piece object
   end
 
 end
