ff5a1c880256ebe1c6b35d852c0a47011071e7b8
Emilio da Costa <emidac@gmail.com>
2017-09-11 14:53:15 -0400

    Error Handling Monster & Friend

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,26 +1,54 @@
+class MonsterError < StandardError
+end
+class BestFriendError < StandardError
+end
+
+require "byebug"
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    puts "#{str} is not an integer"
+    return nil
+  end
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
-def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
-    puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
-end
-
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  begin
+    reaction(maybe_fruit)
+  rescue MonsterError => error
+    # debugger
+    if maybe_fruit == "coffee"
+      retry
+    else
+      puts error.message
+    end
+
+    # puts "How did you know I like coffee!?!"
+    # reaction
+  end
+end
+
+def reaction(maybe_fruit)
+  #debugger
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    # debugger
+    puts MonsterError.new("#{maybe_fruit}'s don't count, but you gave me a coffee, so try again")
+    feed_me_a_fruit
+  else
+    raise MonsterError.new("#{maybe_fruit}'s don't count")
+  end
+end
+
 
 # PHASE 4
 class BestFriend
@@ -28,6 +56,9 @@ class BestFriend
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
+    raise BestFriendError.new("C'mon man - you don't even know your besties name!?") if name.length <= 0
+    raise BestFriendError.new("Whoa! You don't even know your besties fav pastime!?") if fav_pastime.length <= 0
+    raise BestFriendError.new("Only #{yrs_known}!? You can't be besties!") if yrs_known < 5
   end
 
   def talk_about_friendship
@@ -39,8 +70,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
